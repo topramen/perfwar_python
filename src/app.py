38dfd18956db3074f106fae549fd27ffb7a0b735
topramen2 <ramenon@yahoo.com>
2021-10-27 16:24:54 -0500

    added and commented out the single_metric call, to test if this will affect the metric numbers

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ import hashlib
 from os import getenv
 from aws_lambda_powertools import Logger, Tracer, Metrics
 from boto3.dynamodb.conditions import Key
+from aws_lambda_powertools import single_metric
+from aws_lambda_powertools.metrics import MetricUnit
 
 logger = Logger()
 tracer = Tracer()
@@ -19,7 +21,9 @@ table = resource.Table(table_name)
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
-
+    # with single_metric(name="ColdStart", unit=MetricUnit.Count, value=1, namespace="PerfWar") as metric:
+    #     metrics.add_dimension(name="function_name", value=context.function_name)
+    #     metrics.add_dimension(name="version", value="50")
     if event['httpMethod'] == 'POST':
         logger.info("Inside POST Handler")
         return post_handler(event, context)
@@ -43,7 +47,8 @@ def post_handler(event, context):
         table.put_item(Item=payload, ConditionExpression=f"attribute_not_exists(ID)")
     except botocore.exceptions.ClientError as e:
         return {
-            "statusCode": 409,
+            # "statusCode": 409,
+            "statusCode": 209,
             "body": json.dumps({
                 "id": payload['ID'],
                 "message": "object already exists"
@@ -59,10 +64,7 @@ def post_handler(event, context):
 
 def get_handler(event, context):
     id = event['queryStringParameters']['id']
-    # add a try/catch block here
-    # try:
-    # item = table.get_item(Key={'ID': id})
-    # except botocore.exceptions.ResourceNotFoundException as e:
+
     response = table.query(
         KeyConditionExpression=Key('ID').eq(id)
     )
@@ -75,7 +77,8 @@ def get_handler(event, context):
         }
     else:
                 return {
-            "statusCode": 404,
+            # "statusCode": 404,
+            "statusCode": 204,
             "body": json.dumps({
                 "id": id,
                 "message": "object not found"
